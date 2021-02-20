b7286f11503e5eadcf652d6f9b20004cf1f52d0d
widia sereniti <widiasereniti@gmail.com>
2021-02-21 03:16:27 +0700

    update menu-items to navigate withRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Directory from '../../components/directory/directory.component'
 import './homepage.styles.scss'
 
-const Homepage = () => (
+const Homepage = ({ history }) => (
   <div className='homepage'>
-    <Directory />
+    <Directory history={history} />
   </div>
 )
 
